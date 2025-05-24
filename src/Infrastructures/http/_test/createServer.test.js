2de34919e12e7d69ed83d297ca47b010bf429219
onuda22 <okinabada@gmail.com>
2025-05-24 23:45:04 +0700

    test(http:server): write test code for handling error on server

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -37,5 +37,131 @@ describe('HTTP Server', () => {
       expect(responseJson.status).toEqual('success');
       expect(responseJson.data.addedUser).toBeDefined();
     });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // Arange
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'secret',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'
+      );
+    });
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 1,
+        password: true,
+        fullname: ['Dicoding Indonesia'],
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena tipe data tidak sesuai'
+      );
+    });
+
+    it('should response 400 when username more than 50 character', async () => {
+      // Arrange
+      const requestPayload = {
+        username:
+          'asdlfkjweoirjqoejkldasjflakdsjfioqwrq329049i3ijdlfkadsnlfnadlckajdlfjasdkfj',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena karakter username melebihi batas limit'
+      );
+    });
+
+    it('should response 400 when username contain restricted character', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'di cod!ng',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena username mengandung karakter terlarang'
+      );
+    });
+
+    it('should response 400 when usename unavailable', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ username: 'dicoding' });
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('username telah terpakai');
+    });
   });
 });
